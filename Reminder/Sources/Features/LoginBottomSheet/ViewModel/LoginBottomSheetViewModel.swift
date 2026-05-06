bd7152080e2859b72f8671180e36f3997020c1d6
João Gabriel Ramos <joaogabrielramos28@outlook.com>
2026-05-06 19:19:27 -0300

    chore: firebase auth

---
+++ Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  LoginBottomSheetViewModel.swift
+//  Reminder
+//
+//  Created by João Gabriel Ramos on 06/05/26.
+//
+
+import Foundation
+
+class LoginBottomSheetViewModel {
+    func doAuth(usernameLogin:String,password:String){
+        
+    }
+}
