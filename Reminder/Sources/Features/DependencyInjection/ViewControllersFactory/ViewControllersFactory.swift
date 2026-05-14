efe4368b476af483c62c9e94673a629e376287f1
João Gabriel Ramos <joaogabrielramos28@outlook.com>
2026-05-13 21:05:52 -0300

    finish login

---
+++ Reminder/Sources/Features/DependencyInjection/ViewControllersFactory/ViewControllersFactory.swift
@@ -0,0 +1,8 @@
+//
+//  ViewControllersFactory.swift
+//  Reminder
+//
+//  Created by João Gabriel Ramos on 13/05/26.
+//
+
+import Foundation
---
+++ Reminder/Sources/Features/UserDefaults/UserDefaultsManager.swift
@@ -0,0 +1,8 @@
+//
+//  UserDefaultsManager.swift
+//  Reminder
+//
+//  Created by João Gabriel Ramos on 13/05/26.
+//
+
+import Foundation
